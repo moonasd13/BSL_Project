1e8bccdbbf44fb0dfd253cde6a86faaf30513968
moonasd13 <asd4774889@naver.com>
2025-02-24 00:52:10 +0900

    2025.02.23 작업
    캐릭터 컨트롤러, 인벤토리 초반

--- Assets/2.Scripts/PlayerController.cs
+++ Assets/2.Scripts/PlayerController.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    Animator animator;
+    Animator _animator;
+    CharacterController _characterController;
+    float _speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponentInChildren<Animator>();
+        _animator = GetComponentInChildren<Animator>();
+        _characterController = GetComponentInChildren<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    _animator.SetBool("IsRun", true);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    _animator.SetTrigger("IsRoll");
+        //}
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(x, 0, y);
+            _characterController.Move(dir * _speed * Time.deltaTime);
+        if (x != 0 || y != 0)
         {
-            animator.SetBool("IsRun", true);
+            _animator.SetBool("IsRun", true);
+        }
+        else
+        {
+            _animator.SetBool("IsRun", false);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            animator.SetTrigger("IsRoll");
+            _animator.SetTrigger("IsRoll");
         }
     }
 }
